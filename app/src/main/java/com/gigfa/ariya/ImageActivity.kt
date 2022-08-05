9bb3406aebbf8e2d241beeac56c94b939ae295f1
Ariya <ariya2003icdi@gmail.com>
2022-08-05 08:30:32 +0430

    app

--- app/src/main/java/com/gigfa/ariya/ImageActivity.kt
+++ app/src/main/java/com/gigfa/ariya/ImageActivity.kt
@@ -9,21 +9,5 @@ class ImageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image)
 
-        val watermarkText: WatermarkText = WatermarkText(inputText)
-            .setPositionX(0.5)
-            .setPositionY(0.5)
-            .setTextColor(Color.WHITE)
-            .setTextFont(R.font.koodak)
-            .setTextShadow(0.1f, 5, 5, Color.BLUE)
-            .setTextAlpha(150)
-            .setRotation(30)
-            .setTextSize(20)
-
-        WatermarkBuilder
-            .create(this, backgroundBitmap)
-            .loadWatermarkText(watermarkText) // use .loadWatermarkImage(watermarkImage) to load an image.
-            .getWatermark()
-            .setToImageView(imageView);
-
     }
 }
